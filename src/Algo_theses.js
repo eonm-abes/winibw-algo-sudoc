c33973eeb3b6140f97df2b1d1053a7a0b6ea9367
Mathis EON <60341438+eonm-abes@users.noreply.github.com>
2021-07-02 14:44:55 +0200

    feature: add multiple results support

--- src/Algo_theses.js
+++ src/Algo_theses.js
@@ -3,6 +3,7 @@
 // email: eon@abes.fr
 
 const algo_theses_base_url = "https://www.theses.fr/AlgoSudoc";
+const chunk_size = 200;
 
 const winIBWMessageFormat = {
   error: 1,
@@ -11,17 +12,23 @@ const winIBWMessageFormat = {
 };
 
 function AlgoTheses() {
-  let ppn = application.activeWindow.getVariable("P3GPP");
+  let screen_results = application.activeWindow.getVariable("P3VKZ").split("\x1BH\x1BLPP");
+  let ppns = [];
 
-  if (ppn) {
-    let url = urlBuilder("ppn", ppn);
+  for (let i in screen_results) {
+    if (i > 0) {
+      ppns.push(screen_results[i].substr(0, 9));
+    }
+  }
 
-    application.activeWindow.showMessage(
-      `Récupération du rapport d'AlgoThèses depuis : ${url}`,
-      winIBWMessageFormat.notification
-    );
+  ppns = (ppns.length) ? ppns : [application.activeWindow.getVariable("P3GPP")]
 
-    application.shellExecute(`${url}&origin=winibw`, 5, "open", "");
+  if (ppns) {
+    let urls = urlBuilder("ppn", ppns);
+    
+    for (let index in urls) {
+      get_repport(urls[index])
+    }
   } else {
     application.activeWindow.showMessage(
       `Impossible de récupérer le rapport d'AlgoThèses depuis cet écran`,
@@ -30,6 +37,35 @@ function AlgoTheses() {
   }
 }
 
+function get_repport(url) {
+  application.activeWindow.showMessage(
+    `Récupération du rapport d'AlgoThèses depuis : ${url}`,
+    winIBWMessageFormat.notification
+  );
+
+  application.shellExecute(`${url}&origin=winibw`, 5, "open", "");
+}
+
 function urlBuilder(param_name, param_value) {
-  return `${algo_theses_base_url}?${param_name}=${param_value}`;
+  let params = [].concat(param_value || []);
+  let urls = []
+
+  let chunks = chunk(params, chunk_size)
+  
+  for (var i = 0; i < chunks.length; i++) {
+    urls.push(`${algo_theses_base_url}?${param_name}=${chunks[i].join(",")}`)
+  }
+
+  return urls;
+}
+
+function chunk(input, chunk_size) {
+  let chunks = [];
+
+  for (let i = 0, j = input.length; i < j; i += chunk_size) {
+    let temparray = input.slice(i, i + chunk_size);
+    chunks.push(temparray)
+  }
+
+  return chunks
 }
